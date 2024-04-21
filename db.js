852e69c32928e7d14a52acb10d481eabe7420085
victorflourenco <victor.flourenco@outlook.com>
2024-04-21 00:48:15 -0300

    API node com Postgres

---
+++ db.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import postgres from 'postgres'
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+export default sql
---
+++ server.js
@@ -0,0 +1,66 @@
+import {fastify} from 'fastify'
+//import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgre.js'
+const server = fastify()
+
+//const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+
+server.post('/videos', async(request, replay) => {
+  
+  const {title, description, duration} = request.body
+
+  console.log(request.body)
+  
+ await database.create({
+    title,
+    description,
+    duration
+  })
+  
+  replay.status(201).send()
+
+})
+
+server.get('/videos', async (request) => {
+  
+  const search = request.query.search
+  const videos = await database.list(search)
+  console.log(videos)
+
+  return videos
+
+})
+
+server.put('/videos/:id', async (request, replay) => {
+  
+  const videoId = request.params.id
+  const {title, description, duration} = request.body
+
+  console.log(`id do video: ${videoId}`)
+  
+ await database.update(videoId, {
+    title,
+    description,
+    duration
+  })
+  
+ return replay.status(204).send()
+
+})
+
+server.delete('/videos/:id', (request, replay) => {
+  
+  const videoId = request.params.id
+  
+  database.delete(videoId)
+
+  return replay.status(204).send()
+})
+
+
+server.listen({
+    port: process.env.PORT ?? 3334,
+  }
+)
